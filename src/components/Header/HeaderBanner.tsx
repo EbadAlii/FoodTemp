e2c87a34fec9f19cb06741a4fae6b0f14d4b0f28
Ebad <ebad.convicted@gmail.com>
2023-03-26 16:44:53 +0500

    Header Banner Color Changed

--- src/components/Header/HeaderBanner.tsx
+++ src/components/Header/HeaderBanner.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import foodPic from "../../assets/KFC-Food-Plate-Top-View-PNG 2.png";
-import slider from "../../assets/Group 877.png";
 
 function HeaderBanner(props: any) {
   return (
     <div>
-      <div className=" object-cover mt-[13px] h-[138px] w-auto rounded-[20px] bg-[#E414AA] ml-[0px] flex flex-row items-center justify-between text-[18px]">
+      <div className=" object-cover mt-[13px] h-[138px] w-auto rounded-[20px] bg-[#131212] ml-[0px] flex flex-row items-center justify-between text-[18px]">
         <div className="w-[151px] h-auto text-white ml-[28px] leading-[27px]">
           <p className="font-bold">{props.heading}</p>
           <p className="font-light"> {props.details} </p>
